58a514ea6b5f2da61b64046a903f83c0d8a65810
LuckedCoronet <lc45@lc-studios.net>
2026-02-11 08:06:20 +0900

    chore(scripts): make project.ts standalone

--- scripts/project.ts
+++ scripts/project.ts
@@ -1,5 +1,29 @@
 import path from "node:path";
-import { getEnvRequired, parseVersionString } from "./utils";
+
+const getEnvRequired = (key: string, customErrorMsg?: string): string => {
+	const value = process.env[key];
+	if (typeof value !== "string")
+		throw new Error(customErrorMsg ?? `The environment variable '${key}' is required but not set.`);
+	return value;
+};
+
+const parseVersionString = (versionString: string): number[] => {
+	const parts = versionString.split(".");
+	if (parts.length !== 3) {
+		throw new Error(
+			'Version string must contain exactly three integer parts separated by dots (e.g., "1.2.3").',
+		);
+	}
+
+	const numbers = parts.map((part) => {
+		const num = Number(part);
+		if (part.trim() === "" || !Number.isInteger(num)) {
+			throw new Error(`The segment "${part}" is not a valid integer.`);
+		}
+		return num;
+	});
+	return numbers;
+};
 
 export type ProjectConfig = ReturnType<typeof getProjectConfig>;
 
